565bb390163df84a6fecc294b4aeb14c204e10c2
Atheriel <atheriel@gmail.com>
2014-07-08 18:36:35 -0700

    Removed SeededModule for much clearer new() methods.
    Also added some initial tests of the error conditions for Billow
    and Pink noises, and fixed several small documentation errors.
    
    Signed-off-by: Atheriel <atheriel@gmail.com>

--- src/test.rs
+++ src/test.rs
@@ -8,10 +8,23 @@ extern crate grunge;
 
 use cgmath::vector::Vector2;
 use grunge::primitives::snoise_2d;
+use grunge::common::NoiseModule;
+use grunge::fractal::{PinkNoise, BillowNoise};
 
 #[bench]
 fn bench_simplex_noise_2d(b: &mut test::Bencher) {
     b.iter(|| {
     	snoise_2d(Vector2::new(0.05, 0.05), 0u)
     });
+}
+
+#[test]
+fn test_octave_requirements() {
+    let mut pink = PinkNoise::new(0);
+    pink.octaves = 1;
+    let mut billow = BillowNoise::new(0);
+    billow.octaves = 31;
+
+    assert!(pink.generate_2d(Vector2::new(0.05, 0.05)).is_err());
+    assert!(billow.generate_2d(Vector2::new(0.05, 0.05)).is_err());
 }
