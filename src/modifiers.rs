e749e991fc7e3a60bb5b4a971fcce529909e93ef
Atheriel <atheriel@gmail.com>
2014-07-11 23:02:31 -0700

    Added documentation for the modifiers module.
    Signed-off-by: Atheriel <atheriel@gmail.com>

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -2,6 +2,9 @@
     This file is part of grunge, a coherent noise generation library.
 */
 
+//! This module contains types for generating noise by taking a source noise
+//! generator and modifying its output in some way.
+
 use cgmath::vector::Vector2;
 
 use common::NoiseModule;
@@ -16,13 +19,37 @@ pub trait Modifiable : NoiseModule {
     }
 }
 
+/// Modifies a source noise module by bounding its output between a `min` and
+/// `max` value.
+///
+/// ## Example
+/// ClampedNoise can be created one of three ways: with `new()`, which uses the
+/// default min and max values of `-1.0` and `1.0`, respectively; with a struct
+/// literal, or with the `clamp()` method on a source module itself.
+///
+/// ```rust
+/// use grunge::modules::*;
+///
+/// let source = PinkNoise::new(26);
+/// let first_clamp = ClampedNoise::new(source);
+/// let other_clamp = ClampedNoise { source: source, min: -0.5, max: 0.5 };
+/// let final_clamp = source.clamp(-0.5, 0.5);
+/// assert_eq!(other_clamp, final_clamp);
+/// ```
 pub struct ClampedNoise<'a> {
-	pub source: &'a NoiseModule,
-	pub min: f32,
-	pub max: f32,
+    /// The source module.
+    pub source: &'a NoiseModule,
+
+    /// The absolute lower bound for the noise output.
+    pub min: f32,
+
+    /// The absolute upper bound for the noise output.
+    pub max: f32,
 }
 
 impl<'a> ClampedNoise<'a> {
+    /// Creates a new ClampedNoise with a default min and max value of `-1.0`
+    /// and `1.0`, respectively.
     pub fn new(source: &'a NoiseModule) -> ClampedNoise<'a> {
         ClampedNoise { source: source, min: -1.0, max: 1.0 }
     }
@@ -41,13 +68,22 @@ impl<'a> NoiseModule for ClampedNoise<'a> {
 
 impl<'a> Modifiable for ClampedNoise<'a> {}
 
+/// Modifies a source noise module by multiplying its output by a constant and
+/// applying a constant shift up or downwards.
 pub struct ScaledBiasedNoise<'a> {
-	pub source: &'a NoiseModule,
-	pub scale: f32,
-	pub bias: f32,
+    /// The source module.
+    pub source: &'a NoiseModule,
+
+    /// The linear scaling to apply to noise output.
+    pub scale: f32,
+
+    /// The linear transformation to apply to noise output.
+    pub bias: f32,
 }
 
 impl<'a> ScaledBiasedNoise<'a> {
+    /// Creates a new ClampedNoise with a default scale and bias values of
+    /// `1.0` and `0.0`, respectively (i.e. no change in output).
     pub fn new(source: &'a NoiseModule) -> ScaledBiasedNoise<'a> {
         ScaledBiasedNoise { source: source, scale: 1.0, bias: 0.0 }
     }
